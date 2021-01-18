98273ab2953f7ef775fe61f94a74fb4e44493ba1
mariosever <severmario@gmail.com>
2021-01-18 12:48:40 +0100

    Prva verzija flask aplikacije

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
+
+if __name__ == "__main__":
+    app.run()
